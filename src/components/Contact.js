8d720b54948a7146eb405ff2189efef7f2bf249c
Chloe Gimenes <chloegimenes@gmail.com>
2019-10-07 16:00:19 +0200

    lego

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,8 +12,7 @@ function Contact(props) {
             <div className="infos">
                 <p className="text">{props.online ? 'Online' : 'Offline'}</p>
                 <div className="status">
-                    <div className="status-online"></div>
-                    <div className="status-offline"></div>
+
                 </div>
                 
             </div>
